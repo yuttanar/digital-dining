6ee348822b77c1c4efab49b6fccb660604b11550
Yuttanar <yuttanar.tery@gmail.com>
2024-03-06 20:58:59 +0700

    feat: 🎸 _app add liff context and font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { IBM_Plex_Sans_Thai } from "next/font/google";
+import { LiffContextProvider } from "@/contexts/liff.context";
+
+export const ibm_plex = IBM_Plex_Sans_Thai({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["thai", "latin"],
+  display: "swap",
+  fallback: ["Helvetica", "Arial", "sans-serif"],
+});
+
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps) {
+  return (
+    <main className={ibm_plex.className}>
+      <LiffContextProvider >
+        <Component {...pageProps} />
+      </LiffContextProvider>
+    </main>
+  );
 }
+
+export default MyApp;
